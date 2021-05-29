b7ce10c307869f44fb8ad60ce4468ad9fe96448b
Ryan O'Donohue <rod.odonohue@gmail.com>
2021-05-29 10:05:19 -0400

    created all connections in index.js.

--- models/index.js
+++ models/index.js
@@ -11,8 +11,22 @@ Product.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-// Messages.belongsTo(User, {
+Messages.belongsTo(User, {
+  foreignKey: 'user_id'
+});
 
-// })
+User.hasMany(Messages, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Product.hasMany(Messages, {
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE'
+});
+
+Messages.belongsTo(Product, {
+  foreignKey: 'product_id'
+})
 
 module.exports = { User, Product, Messages };

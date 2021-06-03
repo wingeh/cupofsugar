93e6196ab27647a399e177054d47c337b6a7c6b6
Andrew <wing_andrew@hotmail.com>
2021-06-02 21:33:36 -0400

    Expanding handlebars

--- public/js/index.js
+++ public/js/index.js
@@ -1,29 +1,31 @@
-// var ourRequest = new XMLHttpRequest();
-// ourRequest.open('GET', '')
+ var ourRequest = new XMLHttpRequest();
+ ourRequest.open('GET', 'http://localhost:3001/api/products/')
 
-// ourRequest.onload = function(){
-//     if (ourRequest.status >= 200 && ourRequest.status < 400) {
-//         var data = JSON.parce(ourRequest.responseTest);
-//         createHTML (data);
-//     } else {
-//         console.log("We connected to the server, but it returned an error");
-//     }
-// };  
+ourRequest.onload = function(){
+     if (ourRequest.status >= 200 && ourRequest.status < 400) {
+         var data = JSON.parce(ourRequest.responseTest);
+        createHTML (data);
+        console.log(data)
+        console.log("Status 200: Server connected")
+     } else {
+         console.log("We connected to the server, but it returned an error");
+     }
+ };  
 
-// ourRequest.onerror = function() {
-//     console.log("Connection error");
-// };
+ ourRequest.onerror = function() {
+     console.log("Connection error");
+ };
 
-// ourRequest.send();
+ ourRequest.send();
 
-// function createHTML(postingData) {
-//     const rawTemplate = document.getElementById("postTemplate").innerHTML;
-//     const compiledTemplate = Handlebars.compile(rawTemplate);
-//     const ourGeneratedHTML = compiledTemplate(postingData);
+ function createHTML(data) {
+     const rawTemplate = document.getElementById("postTemplate").innerHTML;
+     const compiledTemplate = Handlebars.compile(rawTemplate);
+     const ourGeneratedHTML = compiledTemplate(data);
 
-//     const postContainer = document.getElementById("post-container");
-//     postContainer.innerHTML = ourGeneratedHTML;
-// }
+     const postContainer = document.getElementById("post-container");
+     postContainer.innerHTML = ourGeneratedHTML;
+ }
 
 // Log In / Register overlay
 $(window, document, undefined).ready(function() {

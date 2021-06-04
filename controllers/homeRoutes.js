e11bfa95d9af662d36bea332b2a6bfcfd09b9702
Andrew <wing_andrew@hotmail.com>
2021-06-03 20:07:15 -0400

    Building out pantry functionality

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -12,7 +12,7 @@ router.get('/', async (req, res) => {
         });
         const products = productData.map((product) => product.get({ plain: true }));
         const name = products[0].user.name
-        res.render('homepage', { products, name });
+        res.render('homepage', { products, name, logged_in: req.session.logged_in });
     } catch (err) {
         res.status(500).json(err);
     }
@@ -52,7 +52,7 @@ router.get('/pantry', async (req, res) => {
             where: {user_id: req.session.user_id},
             order:[['updatedAt',  'DESC']]
         });
-        console.log ("Pantry Data: " + pantryData)
+        
         const pantry = pantryData.map((pantry) => pantry.get({ plain: true }));
         console.log(pantry)
         res.render('pantry', { pantry, logged_in: req.session.logged_in });

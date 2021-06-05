d6d6c7378d85b3fba7603c336e02de8f15532b06
Andrew <wing_andrew@hotmail.com>
2021-06-05 10:19:20 -0400

    Fixing bug in log in function

--- controllers/api/user.js
+++ controllers/api/user.js
@@ -16,7 +16,7 @@ router.post('/', async (req, res) => {
 
 router.post('/login', async (req, res) => {
     try {
-      const userData = await User.findOne({ where: {id: req.session.user_id } });
+      const userData = await User.findOne({ where: {email: req.body.email } });
       console.log(userData)
       if (!userData) {
           res.status(400).json({ message: 'Incorrect email or password, please try again' });

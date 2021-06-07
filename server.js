425db4355685fe45b5f48e4a57c6126a5320fcdc
Ryan O'Donohue <rod.odonohue@gmail.com>
2021-06-07 11:01:42 -0400

    tinkering to resolve heroku errors.

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const routes = require('./controllers');
 const exhbs = require('express-handlebars');
 const hbs = exhbs.create({});
 const path = require('path');
-const dotenv = require('dotenv').config()
+require('dotenv').config()
 const sequelize = require('./config/connection');
 const { url } = require('inspector');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
